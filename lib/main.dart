9e74c98537d2222befacb691921a2422a83f407b
Abdul Hannan <abdulhannan8540680@gmail.com>
2024-02-01 17:19:05 +0500

    polygon added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' show pi, sin, cos;
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Rotating Polygon',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -18,6 +21,56 @@ class MyApp extends StatelessWidget {
       home: const HomePage(),
     );
   }
+}
+
+class Polygon extends CustomPainter {
+  final int sides;
+
+  Polygon({required this.sides});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 3;
+
+    final path = Path();
+
+    final center = Offset(size.width / 2, size.height / 2);
+
+    final angle = 2 * pi / sides;
+
+    final angles = List.generate(sides, (index) => index * angle);
+
+    final radius = size.width / 2;
+
+    /*
+      x = center.x + radius * cos(angle)
+      y = center.y + radius * sin(angle)
+    */
+
+    path.moveTo(
+      center.dx + radius * cos(0),
+      center.dy + radius * sin(0),
+    );
+
+    for (var angle in angles) {
+      path.lineTo(
+        center.dx + radius * cos(angle),
+        center.dy + radius * sin(angle),
+      );
+    }
+
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) =>
+      oldDelegate is Polygon && oldDelegate.sides != sides;
 }
 
 class HomePage extends StatefulWidget {
@@ -30,9 +83,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF373D20),
-      body: Center(),
+    return Scaffold(
+      backgroundColor: const Color(0xFF373D20),
+      body: Center(
+        child: CustomPaint(
+          painter: Polygon(sides: 3),
+          child: SizedBox(
+            width: MediaQuery.sizeOf(context).width,
+            height: MediaQuery.sizeOf(context).width,
+          ),
+        ),
+      ),
     );
   }
 }
